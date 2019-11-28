1bad9c29228c48a9a7352979280d6f83baa8d05b
soulkey <397love@gmail.com>
2019-11-29 08:45:20 +0900

    코드 정리
    Bookmark Repository Interface 정리

--- app/src/main/java/com/soulkey/applemint/data/BookmarkRepository.kt
+++ app/src/main/java/com/soulkey/applemint/data/BookmarkRepository.kt
@@ -9,9 +9,9 @@ interface BookmarkRepository{
     fun getBookmarks(): LiveData<List<Bookmark>>
     fun getFbIds(): List<String>
     fun deleteByFbIds(list: List<String>)
-    fun insertAll(list: List<Bookmark>)
+    fun syncWithServer(flag : MutableLiveData<Boolean>)
     fun insert(bookmark: Bookmark)
+    fun insertAll(list: List<Bookmark>)
     fun removeBookmark(id: String)
     fun updateBookmark(bookmark: Bookmark)
-    fun syncWithServer(flag : MutableLiveData<Boolean>)
 }
