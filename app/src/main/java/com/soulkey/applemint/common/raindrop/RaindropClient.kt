96b205c5c8b2aaa0d02d427d327fcc1dce76ab8b
soulkey <397love@gmail.com>
2020-03-12 09:19:51 +0900

    raindrop 일부 코드 수정

--- app/src/main/java/com/soulkey/applemint/common/raindrop/RaindropClient.kt
+++ app/src/main/java/com/soulkey/applemint/common/raindrop/RaindropClient.kt
@@ -8,16 +8,24 @@ import retrofit2.Response
 
 class RaindropClient(private val api: RaindropAPI) {
     fun getCollections() : Single<Response<GetCollectionsResponse>>{
-        return api.getCollections().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+        return api.getCollections()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
 
-    fun createRaindrop(title: String, link: String, tags: List<String>, collectionId: String): Single<Response<CreateRaindropResponse>> {
+    fun createRaindrop(
+        title: String,
+        link: String,
+        tags: List<String>,
+        collectionId: String
+    ): Single<Response<CreateRaindropResponse>> {
         val collectionObject = JsonObject().apply {
             this.addProperty("\$id", collectionId)
         }
         Raindrop(title, link, tags, collectionObject).also {
-            return api.createRaindrop(it).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+            return api.createRaindrop(it)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
         }
-
     }
 }
